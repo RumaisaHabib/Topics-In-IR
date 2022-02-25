648325ac370c69c463b248f370a71c8e54f96b49
Rumaisa Habib <rumaisahabib999@gmail.com>
2022-02-25 17:32:04 +0500

    Minor fixes

--- getsrc.py
+++ getsrc.py
@@ -23,9 +23,6 @@ mobile_emulation = {
         "deviceMetrics": { "width": 360, "height": 640, "pixelRatio": 3.0 },
         "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19" }
 options.add_experimental_option("mobileEmulation", mobile_emulation)
-# options.add_argument('--user-agent=Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19')
-
-# options.binary_location = "/usr/bin/chromium"
 
 # Chrome web driver
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
