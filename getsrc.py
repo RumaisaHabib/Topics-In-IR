e3b0fa062c95f5db56410d91f3ed3d20b24410d1
Rumi <rumaisahabib999@gmail.com>
2022-02-28 17:15:38 +0500

    Screenshot of original page

--- getsrc.py
+++ getsrc.py
@@ -5,6 +5,8 @@ from urllib.parse import urlparse
 import urllib.request, io
 from urllib.error import HTTPError
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 import sys
 from selenium.webdriver.common.by import By
@@ -14,11 +16,13 @@ import pandas as pd
 import json
 from colorama import Fore
 from tqdm import tqdm
+from html2image import Html2Image
+
 ''' 
 STEP ONE:
 Scrape images from the URL
 Sample usage:
-python getsrc.py https://google.com/
+python getsrc.py https://www.google.com (no slash at the end)
 
 '''
 page_data = {}
@@ -47,7 +51,6 @@ options.add_experimental_option("mobileEmulation", mobile_emulation)
 # Chrome web driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options,desired_capabilities=caps)
 
-
 # URL settings, to be replaced with text file of URLs (possible)
 url = sys.argv[1]
 
@@ -60,8 +63,33 @@ if len(host.split("."))>1:
 print("===== HOSTNAME =====")
 page_data["host"] = host
 print(host)
+# Make directory for this website, in case it doesn't exist. If this gives you a syntax error then reformat the website name.
+try:
+    os.system("mkdir -p " + host)
+except:
+    pass
+
 
 driver.get(url)
+
+# save source html
+html = driver.find_element(By.XPATH, '//*')
+html = html.get_attribute('innerHTML')
+html = re.sub("src=\"/", "src=\""+ url + "/",html)
+f = open(host+"/source.html", "w")
+f.write(html)
+f.close()
+
+# take screenshot of original
+# driver.save_screenshot(host+"/original.png")
+
+
+# hti = Html2Image()
+# with open(host+'/source.html') as f:
+#     hti.screenshot(f.read(), save_as='original.png')
+#     os.system("cp original.png " + host + " && rm original.png")
+
+
 
 # Get all elements labelled 'img'
 images = driver.find_elements(By.TAG_NAME, 'img')
@@ -76,11 +104,6 @@ for entry in tqdm(driver.get_log('performance'), bar_format=PROGRESS_BAR):
 print(kb, "KBs")
 page_data["kiloBytesIn"] = kb
 
-# Make directory for this website, in case it doesn't exist. If this gives you a syntax error then reformat the website name.
-try:
-    os.system("mkdir -p " + host)
-except:
-    pass
 
 num_img = 0
 results = pd.DataFrame(columns=("Image Source", "Image Name", "Original Size (KB)"))
@@ -111,17 +134,12 @@ with open(host+"/images.txt", "w") as f:
         # Writes image URL source to a file labelled images.txt in the host directory
 page_data["numImages"] = num_img
 
-'''
-TO DO:
-- Use wget to download all images from the images.txt file into the directory of the host
-- Possible command (for linux):
-'''
 print("===== DOWNLOADING IMAGES =====")
 os.system("cd " + host + " && wget -q --show-progress -i images.txt")
 
 image_names = os.listdir(host)
 for image in image_names:
-    if image == "page_data.json" or image == "results.csv" or image == "images.txt":
+    if image == "page_data.json" or image == "results.csv" or image == "images.txt" or image=="source.html" or image=="original.png":
         continue
     os.system("cd " + host + " && convert " +image +" -define webp:lossless=true " + image.split(".")[0] + ".webp && rm " +image)
 
@@ -132,7 +150,11 @@ json.dump(page_data, f)
 f.close()
 
 results.to_csv(host+"/results.csv")
+# os.system("cd " + host + " && python3 -m http.server 8000")
 
+# IMP: To take screenshots of this page, you have to run python3 -m http.server 8000 in the host folder. Will be put into the bash script
+driver.get('localhost:8000/source.html')
+driver.save_screenshot(host+"/original.png")
 driver.close()
 
 print("===== GET SRC COMPLETE =====")
