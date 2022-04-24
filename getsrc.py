0c92a7933a1e0e1752597896812f60919240e741
RumaisaHabib <rumaisahabib999@gmail.com>
2022-04-24 14:37:14 +0500

    Daraz + ali express + images

--- getsrc.py
+++ getsrc.py
@@ -50,6 +50,7 @@ mobile_emulation = {
         "deviceMetrics": { "width": PHONE_WIDTH, "height": PHONE_HEIGHT, "pixelRatio": PIXEL_RATIO},
         "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19" }
 options.add_experimental_option("mobileEmulation", mobile_emulation)
+options.add_experimental_option("excludeSwitches", ["disable-popup-blocking"])
 
 # Chrome web driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options,desired_capabilities=caps)
