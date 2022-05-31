5015e3cc87261e5753a742436c510029ed9d5047
RumaisaHabib <rumaisahabib999@gmail.com>
2022-05-31 18:57:07 +0500

    Remove images fixed

--- getnew.py
+++ getnew.py
@@ -92,9 +92,6 @@ def try_webp_reduce(image, final_size):
 def reduce_to(image, final_size):
     final_size = final_size*1024
     print(image)
-    # img = iio.imread(host + "/" + image)
-    # os.system("convert " + host + "/" + image + " -quality " + str(5) + "% " + host + "/reduced_"+ image)
-    # smallest_possible_size = os.path.getsize(host + "/reduced_" + image)
     
     # Get original size
     size = os.path.getsize(host + "/" + image)
@@ -102,7 +99,6 @@ def reduce_to(image, final_size):
     # Make new image (reduced)
     org_width, org_height = (Image.open(host + "/" + image)).size
     os.system("cp " + host + "/" + image + " " + host + "/reduced_" + image)
-    
     
     # STEP 1: Reduce quality 
     factor, isFactored, size = reduceQuality(size, final_size, image, "reduced_" +image)
@@ -113,53 +109,23 @@ def reduce_to(image, final_size):
     isBlack = False
     if(size>final_size):
         isBlack = True
-        # img = PIL.Image.open("./" + host + "/reduced_" + image)
-        # info = img.info
-        # img1 = img.convert("RGBA").convert("L")
-        # img1.show()
-        # img1.save("./" + host + "/reduced_" + image, **info)
-        # os.system("convert " + host + "/reduced_" + image +  " -set colorspace Gray -separate -average " + host + "/reduced_" + image)
-        # os.system("convert -colors " + str(colors)  + " " + host + "/reduced_" + image + " " + host + "/reduced_" + image)
         os.system("convert -colors 2 " + host + "/reduced_" + image + " " + host + "/reduced_" + image)
-        
         size = os.path.getsize(host + "/reduced_" + image)
         print(size)
  
         
     os.system("cp " + host + "/reduced_" + image + " " + host + "/copy_reduced_" + image)
-    factor, _, size = reduceQuality(size, final_size, "copy_reduced_" + image, "reduced_" + image)
+    _, _, size = reduceQuality(size, final_size, "copy_reduced_" + image, "reduced_" + image)
     os.system("rm " + host + "/copy_reduced_" + image)
-    # scale = 90
-    # isScaled = False
-    # while(size>final_size):
-    #     isScaled = True
-    #     os.system("convert " + host + "/reduced_" + image + " -scale " + str(scale) + "% " + "-resize "+ str(org_width) + "x" + str(org_height) +" "+ host + "/reduced_"+ image)
-    #     size = os.path.getsize(host + "/reduced_" + image)
-    #     if (size == ((ERROR_MARGIN*final_size)-final_size) or size == ((ERROR_MARGIN*final_size)+final_size)):
-    #         break
-    #     print('scale {} image of size {}'.format(scale,size))
-    #     if scale < 5:
-    #         scale = scale - scale*0.05
-    #     else:
-    #         if(size > final_size):
-    #             scale = math.floor(scale / 2)
-    #         else:
-    #             scale = math.floor(0.75*2*scale)
     
-        # scale = scale - scale*0.05
+    # STEP 3: Remove image
     isRemoved = False
     if(size>final_size):
-        
         isRemoved = True
-        # img = PIL.Image.open("./" + host + "/reduced_" + image)
-        # img1 = img.convert('RGBA').convert("P", palette=PIL.Image.ADAPTIVE, colors=1)
-        # img1.save("./" + host + "/reduced_" + image)
-        os.system("convert -size "+ str(org_width)+ "x" + str(org_height) +" xc:transparent " + host + "/reduced_" + image)
 
     if not isFactored:
         factor = 100
-    # if not isScaled:
-    #     scale = 100
+        
     end_size = os.path.getsize(host + "/reduced_" + image)
     if isRemoved:
         end_size = 0
@@ -190,12 +156,10 @@ for image in tqdm(image_names, bar_format=PROGRESS_BAR):
         
         to_replace = results.loc[image,"Image Source"]
         if (removed):
-            # html_list = html.split()
-            # insert_at = html_list.index("src=\""+ to_replace + "\"") + 1
-            # html_list[insert_at:insert_at] = ["style=display:none"]
-            # html = " ".join(html_list)
             src_str = "src=\""+ to_replace + "\""
-            html = html.replace(src_str, src_str + " style=display:none")
+            replace_with = src_str + " style=\"display:none\""
+            html = html.replace(src_str, "")
+            print(replace_with)
             continue
             
         print(to_replace)
@@ -203,16 +167,9 @@ for image in tqdm(image_names, bar_format=PROGRESS_BAR):
         
         html = html.replace(to_replace, replace_with)
         print("replaced")
-        # if removed:
-        #     to_replace = "https://localhost:4696/"+host+"/reduced_"+ image + "\" "
-        #     replace_with = "https://localhost:4696/"+host+"/reduced_"+ image + "\" " + "style=\"display:none\""
         sys.stdout.flush()
     except Exception as e:
         print("ERROR:",e)
-
-# f = open(host+"/reduced.html", "w")
-# f.write(html)
-# f.close()
 
 
 # Set options for web scraper
@@ -234,15 +191,6 @@ options.add_experimental_option("mobileEmulation", mobile_emulation)
 # Chrome web driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options,desired_capabilities=caps)
 driver.get(url)
-
-
-f = open(host+"/2.txt", "w")
-f.write(driver.execute_script("return document.body.innerHTML"))
-f.close()
-
-# html = html.replace("srcset=")
-
-
 driver.execute_script("document.body.innerHTML = arguments[0]", html)
 
 # Manually remove srcset, forcing it use the image we give
@@ -254,12 +202,6 @@ f = open(host+"/reduced.html", "w")
 f.write(driver.page_source)
 f.close()
 
-# html_file = os.getcwd() + "//" + host + "//reduced.html"
-# driver.get("file:///" + html_file)
-# html = re.sub("(<html[^<]+>)", "", html)
-# html = html.replace("<html>", "")
-# html = html.replace("</html>", "")
-# driver.refresh()
 time.sleep(10)
 driver.save_screenshot(host+"/reduced.png")
 driver.close()
