f0946a7880843aedb740a1b7da03785dd0b014f0
RumaisaHabib <rumaisahabib999@gmail.com>
2022-07-13 22:49:19 +0500

    Initial QSS testing + requirements merge

--- test.sh
+++ test.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-for i in {1..3}
+for i in {1..2}
 do
-    rm -r www.daraz.pk
-    bash affordability_report https://www.daraz.pk 0.80 0.1 0.1 0.1 0.1
+    #rm -r www.daraz.pk
+    #bash affordability_report https://www.daraz.pk 0.80 0 0 0 0.1
+    #rm -r www.daraz.pk
+    #bash affordability_report https://www.daraz.pk 0.80 0 0.01 0 0.1
+    #rm -r www.daraz.pk
+    #bash affordability_report https://www.daraz.pk 0.80 0 0.01 0.01 0.1
+    #rm -r www.daraz.pk
+    #bash affordability_report https://www.daraz.pk 0.80 0.01 0.01 0 0.1
+    #rm -r www.daraz.pk
+    #bash affordability_report https://www.daraz.pk 0.80 0.01 0.01 0.01 0.1
     
-    bash affordability_report https://www.daraz.pk 0.80 0.1 0.1 0.1 0
-    rm -r www.daraz.pk
-    bash affordability_report https://www.daraz.pk 0.80 0.1 0.1 0 0.1
-    rm -r www.daraz.pk
-    bash affordability_report https://www.daraz.pk 0.80 0.1 0 0.1 0.1
-    rm -r www.daraz.pk
-    bash affordability_report https://www.daraz.pk 0.80 0 0.1 0.1 0.1
-    rm -r www.daraz.pk
-    
-    bash affordability_report https://www.daraz.pk 0.80 0 0 0 0.1
+
     rm -r www.daraz.pk
     bash affordability_report https://www.daraz.pk 0.80 0 0.1 0 0.1
     rm -r www.daraz.pk
-    bash affordability_report https://www.daraz.pk 0.80 0 0.1 0.1 0.1
+    bash affordability_report https://www.daraz.pk 0.80 0 0.1 0.01 0.1
+    rm -r www.daraz.pk
+    bash affordability_report https://www.daraz.pk 0.80 0.01 0.1 0 0.1
+    rm -r www.daraz.pk
+    bash affordability_report https://www.daraz.pk 0.80 0.01 0.1 0.01 0.1
+    
 done
